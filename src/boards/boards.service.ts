74d067ffb9c41d9f86e190b5722d8b7003e17cf6
JuMoth <tkdlqjqkd@gmail.com>
2024-08-07 17:29:41 +0900

    feat: board-model define

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-  private boards = ['특정값'];
+  private boards: Board[] = [];
 
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boards;
   }
 }
